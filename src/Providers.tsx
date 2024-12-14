008598ac341733b74b7fedfd03882eb5b5742fca
Rithrita-Kong <kongrithrita@gmail.com>
2024-12-14 20:32:02 +0700

    feat: Add vercel analytics

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { SessionProvider } from "@/context/SessionContext";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 const Providers = () => {
   return (
     <SessionProvider>
       <div>
-        <Toaster position="top-right" /> {/* Toast container */}
+        <Toaster position="top-right" />
         <Outlet />
+        <Analytics />
       </div>
     </SessionProvider>
   );
